50d25bf551fa6ea7d02ad4975be42472ae201423
Yishay Hazan <ishay@saitechs.io>
2023-11-20 12:12:28 +0200

    fixes

--- src/screens/home/parts/hero/index.tsx
+++ src/screens/home/parts/hero/index.tsx
@@ -5,20 +5,20 @@ import { Link } from "~/components/link";
 export const HeroSection = component$(() => {
   return (
     <section class={styles.hero} id="hero">
+      <img src="/gallery/5.webp" alt="" />
       <div class={styles.content}>
         <h1>בית המצבר - מצבר לרכב במשלוח עד הבית</h1>
         <p>
-          תקועים בדרכים? הרכב לא מניע? נתקעתם בלי מצבר? אנחנו מבית המצבר נגיע עד
-          אליכם עם המצבר שאתם צריכים, בכל זמן - שירות מקצועי ואדיב, ומחיר הוגן.
+          תקועים בדרכים? הרכב לא מניע? נתקעתם בלי מצבר?
+          <br />
+          ניידת שירות תגיע עד אליכם עם המצבר המדוייק שרכבכם זקוק לו - שירות
+          מקצועי ואדיב 24/6 באיזור ירושלים והסביבה
         </p>
         <div>
           <Link href="#callme" active>
             צרו קשר
           </Link>
         </div>
-      </div>
-      <div class={styles.thumbnail}>
-        <img src="/battery-bubble.png" alt="battery" width={150} height={150} />
       </div>
     </section>
   );

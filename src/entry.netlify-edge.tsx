10c3a8a11a828813264722787b72b896fb1e2ed9
yishayhaz <yishayhaz@gmail.com>
2024-11-21 18:15:09 +0200

    tset prod

--- src/entry.netlify-edge.tsx
+++ src/entry.netlify-edge.tsx
@@ -1,22 +1,5 @@
-/*
- * WHAT IS THIS FILE?
- *
- * It's the entry point for Netlify Edge when building for production.
- *
- * Learn more about the Netlify integration here:
- * - https://qwik.builder.io/docs/deployments/netlify-edge/
- *
- */
-import {
-  createQwikCity,
-  type PlatformNetlify,
-} from "@builder.io/qwik-city/middleware/netlify-edge";
+import { createQwikCity } from "@builder.io/qwik-city/middleware/netlify-edge";
 import qwikCityPlan from "@qwik-city-plan";
-import { manifest } from "@qwik-client-manifest";
 import render from "./entry.ssr";
 
-declare global {
-  interface QwikCityPlatform extends PlatformNetlify {}
-}
-
-export default createQwikCity({ render, qwikCityPlan, manifest });
+export default createQwikCity({ render, qwikCityPlan });
